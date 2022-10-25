e5aec26498eee795eaf7e1c1fb56fbd965519204
gleitons <31367377+gleitons@users.noreply.github.com>
2022-10-25 07:34:42 -0300

    add jhess

---
+++ codigo.js
@@ -0,0 +1,6 @@
+const nomeProduto = document.getElementById('produto').innerHTML;
+
+document.getElementById('mostraF').innerHTML = `<form action="" method="post">
+<input type="text" name="" placeholder="nome" id="nome">
+<input type="text" name="" placeholder="${nomePoduto}" id="">
+</form>`;
